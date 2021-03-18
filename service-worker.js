b27336d2b416a3d24000699da4bd4c0d99595fb7
Taras Chirkov <chirkovtaras@gmail.com>
2021-03-18 17:55:34 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.cd9625f0b560bdad00a8fd5daa0c6701.js"
 );
 
 self.addEventListener('message', (event) => {
